0c9b97201c5b2da9792328128438691f57de66f2
nagma@NAZ <nagmanaz1101@gmail.com>
2022-09-18 15:06:09 +0530

    new file divided add

---
+++ div.py
@@ -0,0 +1,10 @@
+# This program adds div numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add div numbers
+div = num1 / num2
+
+# Display the div
+print('The sub of {0} and {1} is {2}'.format(num1, num2, div))
