c987627fe15b2927b93a98eee15f54b3f6165e6c
nagma@NAZ <nagmanaz1101@gmail.com>
2022-09-18 17:15:22 +0530

    name change

--- mup.py
+++ mup.py
@@ -3,8 +3,8 @@
 num1 = 50
 num2 = 5
 
-# mup sub numbers
-sub = num1 * num2
+# mup mup numbers
+mup = num1 * num2
 
 # Display the mup
 print('The mup of {0} and {1} is {2}'.format(num1, num2, mup))
