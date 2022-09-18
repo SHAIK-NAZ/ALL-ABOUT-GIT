23a5d7d8e683cd0f6e42f0347f975a58c66b5c3d
nagma@NAZ <nagmanaz1101@gmail.com>
2022-09-18 17:34:29 +0530

    changes done

--- div.py
+++ div.py
@@ -7,4 +7,4 @@ num2 = 6.3
 div = num1 / num2
 
 # Display the div
-print('The sub of {0} and {1} is {2}'.format(num1, num2, div))
+print('The div of {0} and {1} is {2}'.format(num1, num2, div))
---
+++ mup.py
@@ -0,0 +1,10 @@
+# This program adds mup numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add mup numbers
+mup = num1 * num2
+
+# Display the div
+print('The mup of {0} and {1} is {2}'.format(num1, num2, mup))
